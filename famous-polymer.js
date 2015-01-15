64c50c054719727766d72bbd04c8e6d258ba1f59
Taehoon Moon <panarch@kaist.ac.kr>
2015-01-15 08:48:27 +0000

    Added browserify support

---
+++ famous-polymer.js
@@ -0,0 +1,8 @@
+module.exports = {
+    CoreSelector: require('./src/CoreSelector'),
+    PaperCheckbox: require('./src/PaperCheckbox'),
+    PaperButton: require('./src/PaperButton'),
+    PaperDropdownMenu: require('./src/PaperDropdownMenu'),
+    PaperSlider: require('./src/PaperSlider'),
+    Util: require('./src/Util')
+};
